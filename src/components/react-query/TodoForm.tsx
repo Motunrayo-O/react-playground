75142498f9019ddb98777091528010f472981946
motunrayo-o <motunrayo.ogunyinka@gmail.com>
2023-10-04 16:33:39 +0100

    refactor to create useAddTodos hook

--- src/components/react-query/TodoForm.tsx
+++ src/components/react-query/TodoForm.tsx
@@ -1,61 +1,16 @@
 import { useRef } from "react";
-import { Todo } from "../../hooks/useTodos";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import axios from "axios";
-
-interface AddTodoContext {
-  todosBeforeUpdate: Todo[];
-}
+import useAddTodos from "../../hooks/useAddTodos";
 
 const TodoForm = () => {
-  const queryClient = useQueryClient();
-
-  const addNewTodo = useMutation<Todo, Error, Todo, AddTodoContext>({
-    mutationFn: (todo: Todo) =>
-      axios
-        .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
-        .then((res) => res.data),
-    onMutate: (todoToCreate: Todo) => {
-      const todosBeforeUpdate =
-        queryClient.getQueryData<Todo[]>(["todos"]) || [];
-
-      // optimistic update
-      queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
-        todoToCreate,
-        ...(todos || []),
-      ]);
-
-      // clear the input field
-      if (ref.current) ref.current.value = "";
-
-      return { todosBeforeUpdate };
-    },
-    onSuccess: (createdTodo, todoToCreate) => {
-      console.log(createdTodo);
-
-      // Invalidate the cache to trigger a refresh
-      // queryClient.invalidateQueries({
-      //   queryKey: ["todos"],
-      // });
-
-      //JSON Placeholder does not actually add the item so update cache manually
-      queryClient.setQueryData<Todo[]>(["todos"], (todos) =>
-        todos?.map((t) => (t === todoToCreate ? createdTodo : t))
-      );
-    },
-    onError: (error: Error, todoToCreate, context) => {
-      if (!context) return;
-
-      // restore the data to state before update attempt
-      queryClient.setQueryData(["todos"], context.todosBeforeUpdate);
-    },
-  });
   const ref = useRef<HTMLInputElement>(null);
+  const addTodo = useAddTodos(() => {
+    if (ref.current) ref.current.value = "";
+  });
 
   return (
     <>
-      {addNewTodo.error && (
-        <div className="alert alert-danger">{addNewTodo.error.message}</div>
+      {addTodo.error && (
+        <div className="alert alert-danger">{addTodo.error.message}</div>
       )}
       <form
         className="row mb-3"
@@ -63,7 +18,7 @@ const TodoForm = () => {
           event.preventDefault();
 
           if (ref.current && ref.current.value)
-            addNewTodo.mutate({
+            addTodo.mutate({
               id: 0,
               title: ref.current?.value,
               completed: false,
@@ -75,8 +30,8 @@ const TodoForm = () => {
           <input ref={ref} type="text" className="form-control" />
         </div>
         <div className="col">
-          <button className="btn btn-primary" disabled={addNewTodo.isLoading}>
-            {addNewTodo.isLoading ? "Adding..." : "Add"}
+          <button className="btn btn-primary" disabled={addTodo.isLoading}>
+            {addTodo.isLoading ? "Adding..." : "Add"}
           </button>
         </div>
       </form>
---
+++ src/hooks/useAddTodos.ts
@@ -0,0 +1,56 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { Todo } from "./useTodos";
+import axios from "axios";
+import { CACHE_KEY_TODOS } from "../components/react-query/constants";
+
+interface AddTodoContext {
+  todosBeforeUpdate: Todo[];
+}
+
+const useAddTodos = (onAdd: () => void) => {
+  const queryClient = useQueryClient();
+
+  const addNewTodo = useMutation<Todo, Error, Todo, AddTodoContext>({
+    mutationFn: (todo: Todo) =>
+      axios
+        .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
+        .then((res) => res.data),
+    onMutate: (todoToCreate: Todo) => {
+      const todosBeforeUpdate =
+        queryClient.getQueryData<Todo[]>(CACHE_KEY_TODOS) || [];
+
+      // optimistic update
+      queryClient.setQueryData<Todo[]>(CACHE_KEY_TODOS, (todos) => [
+        todoToCreate,
+        ...(todos || []),
+      ]);
+
+      onAdd();
+
+      return { todosBeforeUpdate };
+    },
+    onSuccess: (createdTodo, todoToCreate) => {
+      console.log(createdTodo);
+
+      // Invalidate the cache to trigger a refresh
+      // queryClient.invalidateQueries({
+      //   queryKey: CACHE_KEY_TODOS,
+      // });
+
+      //JSON Placeholder does not actually add the item so update cache manually
+      queryClient.setQueryData<Todo[]>(CACHE_KEY_TODOS, (todos) =>
+        todos?.map((t) => (t === todoToCreate ? createdTodo : t))
+      );
+    },
+    onError: (error: Error, todoToCreate, context) => {
+      if (!context) return;
+
+      // restore the data to state before update attempt
+      queryClient.setQueryData(CACHE_KEY_TODOS, context.todosBeforeUpdate);
+    },
+  });
+
+  return addNewTodo;
+};
+
+export default useAddTodos;
