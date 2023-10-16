a2576b71050b9dd726f5480391daae86da4727c0
motunrayo-o <motunrayo.ogunyinka@gmail.com>
2023-10-16 12:56:06 +0100

    install zustand dev tools

--- src/components/state-management/counter/store.ts
+++ src/components/state-management/counter/store.ts
@@ -1,15 +1,21 @@
 import { create } from "zustand";
+import { mountStoreDevtool } from "simple-zustand-devtools";
 
 interface CounterStore {
   value: number;
+  max: number;
   increment: () => void;
   reset: () => void;
 }
 
 const useCounterStore = create<CounterStore>((set) => ({
   value: 0,
+  max: 5,
   increment: () => set((store) => ({ value: store.value + 1 })),
   reset: () => set(() => ({ value: 0 })),
 }));
+
+if (process.env.NODE_ENV == "development")
+  mountStoreDevtool("Counter Store", useCounterStore);
 
 export default useCounterStore;
